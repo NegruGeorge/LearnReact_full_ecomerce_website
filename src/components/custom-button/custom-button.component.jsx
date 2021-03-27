b5349a4287ea4e85c522b6bcb474f7c03dd7387e
Negru George <negru.george8@yahoo.com>
2021-03-27 12:59:17 +0200

    first push

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './custom-button.styles.scss';
+const CustomButton = ({children,isGoogleSignIn,...otherPorps})=>(
+
+<button className={`${isGoogleSignIn ? 'google-sign-in' : '' }  custom-button `}
+  {...otherPorps} 
+ 
+ >
+    {children}
+</button>
+
+)
+
+export default CustomButton;
---
+++ src/firebase/firebase.util.js
@@ -0,0 +1,36 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config ={
+    apiKey: "AIzaSyCNr-phG480bWjZ-9YnmDRRVvTOvkS-Wp0",
+    authDomain: "crwn-db-706f2.firebaseapp.com",
+    projectId: "crwn-db-706f2",
+    storageBucket: "crwn-db-706f2.appspot.com",
+    messagingSenderId: "646858301421",
+    appId: "1:646858301421:web:d06f1a31788f4a9e6311e4",
+    measurementId: "G-9GNQRZEKDG"
+  };
+
+export const createUserProfileDocument = async (userAuth, additionalData) =>
+{
+    if(!userAuth) return; 
+
+    console.log(firestore.doc('users/1234123asd'))
+
+}
+
+
+firebase.initializeApp(config);
+export const auth = firebase.auth();
+
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt:'select_account'});
+
+export const signInWithGoogle = ()=> auth.signInWithPopup(provider);
+
+export default firebase;
+
